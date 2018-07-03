90ff2e2550452de4165e8432ccef61f13629fe40
Dillon Means <dillon.j.means@gmail.com>
2018-07-03 11:39:59 -0600

    added index route

--- tower-backend/index.js
+++ tower-backend/index.js
@@ -4,3 +4,7 @@ var port = process.env.PORT || 3000
 app.listen(port)
   .on('error', console.error.bind(console))
   .on('listening', console.log.bind(console, "listening on "+ port))
+
+  app.get('/', function(req, res, next){
+    res.sendStatus(200);
+});
