e284f8df3a94a886c0fc7c007755ece5f213f0c7
Dillon Means <dillon.j.means@gmail.com>
2018-07-03 11:52:41 -0600

    added index route

--- tower-backend/app.js
+++ tower-backend/app.js
@@ -4,26 +4,20 @@ const cors = require("cors");
 const app = express();
 var varietals = require('./routes/varietals');
 var regions = require('./routes/regions');
+var logger = require("morgan")
 
 
 app.use(bodyParser.json());
+app.use(logger('dev'))
 app.use(cors())
 
 app.get('/', function(req, res, next){
     res.sendStatus(200);
 });
 
-
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header('Access-Control-Allow-Headers', 'Authorization');
-    next();
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', )
 app.use('/varietals', varietals);
 app.use('/regions', regions);
 
